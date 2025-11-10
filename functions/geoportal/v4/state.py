3be608bf3517bf9d7ada4fe281281cbebe893dda
tingli-git <ting.li@kaust.edu.sa>
2025-11-10 15:59:37 +0300

    added local xyz tile

--- functions/geoportal/v4/state.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-import solara
-
-class ReactiveRefs:
-    """Holds shared refs between hooks (selected marker, etc.)."""
-    def __init__(self):
-        self.active_marker_ref = solara.use_ref(None)
-        self.did_fit_ref = solara.use_ref(False)
